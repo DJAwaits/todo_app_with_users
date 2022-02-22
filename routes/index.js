784f2ac139a5722052fdb0c77276722ca98bc2ca
DJAwaits <padjcreeper@gmail.com>
2022-02-22 08:55:10 -0800

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 const todoController = require('../controllers/todoController');
+const userController = require('../controllers/userController');
 
 
 
@@ -10,15 +11,17 @@ const todoController = require('../controllers/todoController');
 router.get('/',  todoController.listAll);
 
 
-router.get('/item/add',  todoController.displayAddItem);
-router.post('/item/add',  todoController.addNewItem);
+router.get('/item/add', todoController.displayAddItem);
+router.post('/item/add', todoController.addNewItem);
 
-router.get('/item/edit/:id',  todoController.viewEditItem);
-router.post('/item/edit/:id',  todoController.saveEditItem);
+router.get('/item/edit/:id', todoController.viewEditItem);
+router.post('/item/edit/:id', todoController.saveEditItem);
 
-router.get('/item/delete/:id',  todoController.deleteItem);
-router.get('/item/complete/:id',  todoController.makeItemComplete);
-router.get('/item/incomplete/:id',  todoController.markItemIncomplete);
+router.get('/item/delete/:id', todoController.deleteItem);
+router.get('/item/complete/:id', todoController.makeItemComplete);
+router.get('/item/incomplete/:id', todoController.markItemIncomplete);
+router.get('/register', userController.renderRegistration);
+router.post('/register', userController.register);
 
 
 module.exports = router;
